20e1d2f135c7dea51dfe6948ad8a6f4be850bb6a
Ad_Victoriam <keeper24@gmail.com>
2018-08-06 19:14:12 -0700

    so much Javascript, added card generation/deletion. working on enemy cards.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,20 +1,66 @@
 $(document).ready(function() {
     //Intro variables held pretty much forever.
     var gameChosen = true;
+    var heroPicked = false;
     var heroChosen = "";
+    var numberOfFiends = (Math.floor(Math.random()*2)+1)
+    var rouge = {
+        health: 100,
+        speed: 15,
+        strength: 3,
+        ability: "Backstab"
+
+    };
+    var fighter = {
+        health: 125,
+        speed: 8,
+        strength: 8,
+        ability: "Stun"
+    }
+    var whiteMage = {
+        health: 75,
+        speed: 10,
+        strength: 2,
+        ability: "Heal"
+    }
+    var tank = {
+        health: 175,
+        speed: 5,
+        strength: 5,
+        ability: "Block"
+    }
+    var enemyType = {
+        health: Math.floor(Math.random()*100)+50,
+        speed: Math.floor(Math.random()*8)+5,
+        strength: Math.floor(Math.random()*4)+5,
+        ability: "shade"
+    }
+    console.log(enemyType)
     // starts the game, also can be used to reset. 
     function initializeGame(){
         $(".hero").on('click' , function(){
             heroChosen = $(this).attr("value");
-            this.detach().appendTo('#heroClassDiv')
+            heroPicked = true;
             console.log(heroChosen)
-
-            if(heroChosen === "Rouge"){
-            }
-
-            
+            $(this).addClass("player").removeClass("hero")
+            $(this).clone().appendTo("div.heroClassDiv")
+            $(this).hide();
+            $(".hero").hide(500);
+            var $btn = $("<button>",{id:"button","class":"attack btn"});
+            $btn.text("Attack!");
+            $btn.click(function () {
+                var roll = $('#button-str').val()
+                var attack = Math.floor(Math.random()*10)+roll;
+                console.log(attack)
+            });
+            $(".player").append($btn)
         });
     }
-    initializeGame();
+    // function generateEnemy(){
+    //     $(".card").clone().appendTo("div.enemyDiv")
+    // };
+
+initializeGame();
+generateEnemy();
 
 })
